577bc3bf8676492efe528f7e8acfb7ece77a4e29
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-08-09 19:38:32 -0700

    added TwoSumII

--- TwoSumII.py
+++ TwoSumII.py
@@ -1,6 +1,8 @@
 class Solution:
+    
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # results = []
+        
+        # # Strategy 1
         # for i in range(len(numbers)):
         #     for j in range(len(numbers)):
         #         if i!=j and numbers[i] + numbers[j] == target:
@@ -8,13 +10,26 @@ class Solution:
         #             results.append(j+1)
         #             return results
 
-        i = 0 
+        # # Strategy 2
         j = len(numbers) - 1
+    
+        for i in range(len(numbers)):
+            while i < j and numbers[i] + numbers[j] > target:
+                j -= 1
+            if i < j and numbers[i] + numbers[j] == target:
+                return [i + 1, j + 1]
 
-        while i < j:
-            if numbers[i] + numbers[j] < target:
-                i+=1
-            if numbers[i] + numbers[j] > target:
-                j-=1
-            if numbers[i] + numbers[j] == target:
-                return [i+1,j+1]
+                if j < len(numbers) - 1:
+                    j += 1
+
+        # # Strategy 3
+        # i = 0 
+        # j = len(numbers) - 1
+
+        # while i < j:
+        #     if numbers[i] + numbers[j] < target:
+        #         i+=1
+        #     if numbers[i] + numbers[j] > target:
+        #         j-=1
+        #     if numbers[i] + numbers[j] == target:
+        #         return [i+1,j+1]
