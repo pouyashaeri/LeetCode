b83d300fee9eec0c307c003a8a4d99a33808d520
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-06 20:30:08 -0700

    group anagrams

---
+++ GroupAnagrams.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+        
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key not in res:
+                res[key] = []
+
+            res[key].append(word)
+
+        return list(res.values())
