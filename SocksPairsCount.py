723a41ed48dbaeb6fd426e4fedbfa6b5a486d55d
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-06 20:52:37 -0700

    socks pairs count

---
+++ SocksPairsCount.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    dic = {}
+    
+    for i in ar:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    
+    pairs = 0
+    
+    for i in dic:
+        if dic[i]//2 > 0:
+            pairs += dic[i]//2
+    
+    return pairs
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
