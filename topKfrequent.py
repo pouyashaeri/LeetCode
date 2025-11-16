4762876573050de21b90ed62665b79bde8408ebb
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-16 15:33:30 -0700

    top k frequent

--- topKfrequent.py
+++ topKfrequent.py
@@ -1,16 +1,15 @@
-nums = [1,1,2,2,2,2,2,3,3]
-k = 2
+from typing import List
 
-freq = {}
-for n in nums:
-    if n in freq:
-        freq[n] += 1
-    else:
-        freq[n] = 1
-
-# Step 2: get keys sorted by their frequency (highest first)
-# sorted(dict, key=dict.get, reverse=True) sorts keys by values
-sorted_keys = sorted(freq, key=freq.get, reverse=True)
-
-# Step 3: return first k keys
-print(sorted_keys[:k])
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # ex_in: [1,5,3,1,2,3,2,2,3,4,6,6,6], k = 3
+        # ex_out: [6,3,2]
+        counts = {}
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+        top_k = []
+        for _ in range(k):
+            key_max = max(counts, key=counts.get)
+            max_value = counts.pop(key_max)
+            top_k.append(key_max)
+        return top_k
