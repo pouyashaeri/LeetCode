9665a42bc54d1b6cfeaf9fec75c2683932d1a2c2
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-24 01:30:35 -0700

    Longest Substring Without Repeating Characters

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -2,16 +2,13 @@ from typing import List
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_substr = ""
-        for i in range(len(s)):
-            substr = ""
-            left = i
-            while s[left] not in substr:
-                substr += substr.join(s[left])
-                if left < len(s) - 1:
-                    left +=1
-                if left > len(s):
-                    break
-            if len(max_substr) < len(substr):
-                max_substr = substr
-        return len(max_substr)
+        seen = set()
+        left = 0
+        max_len = 0
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_len = max(max_len, right - left + 1)
+        return max_len
