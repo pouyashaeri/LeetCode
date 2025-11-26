1c29741e07f076a1f94de4b465548496e6f30a9d
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-25 20:30:10 -0700

    Valid Parentheses

---
+++ ValidParentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        if len(s) <= 1:
+            return False
+
+        stack = []
+
+        for ch in s:
+            if ch in "([{":
+                stack.append(ch)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    if ch == ")":
+                        if(stack.pop()!="("):
+                            return False
+                    if ch == "]":
+                        if(stack.pop()!="["):
+                            return False
+                    if ch == "}":
+                        if(stack.pop()!="{"):
+                            return False
+
+        return True if not stack else False
