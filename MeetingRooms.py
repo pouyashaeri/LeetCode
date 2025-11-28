e119ac7cdb789b4ff7e7989d95b0933a7baec4ba
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-11-27 18:36:27 -0700

    Meeting Rooms

---
+++ MeetingRooms.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals = sorted(intervals, key=lambda x:x.start)
+        for i in range(1, len(intervals)):
+            if intervals[i-1].end > intervals[i].start:
+                return False
+        return True
