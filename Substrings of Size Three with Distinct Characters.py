3ded36c18fd6b6406a8e4e1afbe1f9e6e57ddd5d
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-12-01 02:07:07 -0700

    Substrings of Size Three with Distinct Characters.py

--- Substrings of Size Three with Distinct Characters.py
+++ Substrings of Size Three with Distinct Characters.py
@@ -1,29 +1,14 @@
 class Solution(object):
-    def maxVowels(self, s, k):
+    def countGoodSubstrings(self, s):
         """
         :type s: str
-        :type k: int
         :rtype: int
         """
-        left = 0
-        vowels = {'a','e','i','o','u'}
+        count = 0
         
-        window = s[:k]
-
-        count_window = 0
-
-        for ch in window:
-            if ch in vowels:
-                count_window += 1
-
-        count_max_window = count_window
-        
-        for right in range(k, len(s)):
-            if s[right] in vowels:
-                count_window += 1 
-            if s[left] in vowels:
-                count_window -= 1
-            count_max_window = max(count_max_window, count_window)
-            left += 1
-
-        return count_max_window
+        for i in range(len(s) - 2):
+            window = s[i:i + 3]
+            if len(set(window)) == 3:
+                count += 1
+            
+        return count
