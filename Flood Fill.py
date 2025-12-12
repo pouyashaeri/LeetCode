507bdf2bc9a1ab59b0718462cb6f8a2f1b895261
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-12-11 22:25:48 -0700

    Flood Fill

---
+++ Flood Fill.py
@@ -0,0 +1,27 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        original = image[sr][sc]
+
+        if original == color:
+            return image
+
+        image[sr][sc] = color
+
+        queue = deque([(sr, sc)])
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while queue:
+            r, c = queue.popleft()
+
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < m and 0 <= nc < n and image[nr][nc] == original:
+                    image[nr][nc] = color
+                    queue.append((nr, nc))
+        
+        return image
