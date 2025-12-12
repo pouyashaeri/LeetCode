de9954683a4f96a0801fb3d09e1d47ec4f6ee77b
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-12-11 22:50:38 -0700

    Number of Islands BFS

---
+++ Number of Islands BFS.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        if not grid:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        count = 0
+
+        def bfs(r, c):
+            grid[r][c] = "0"
+            queue = deque([(r, c)])
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == "1":
+                        grid[nr][nc] = "0"
+                        queue.append((nr, nc))
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    bfs(i, j)
+                
+        return count
