356fa8a55947d923f85ed527b6959f44e9afbdd5
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-12-20 02:05:09 -0700

    House Robber II

---
+++ HouseRobberII.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+        def rob1(nums):
+            prev1, prev2 = 0, 0
+            for num in nums:
+                prev1, prev2 = max(num + prev2, prev1), prev1
+            return prev1
+        return max(rob1(nums[1:]), rob1(nums[:len(nums)-1]))
