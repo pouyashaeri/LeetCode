901424a701c93954b04177e6de3d5af082608aa2
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2026-01-05 01:00:01 -0700

    top k elements

--- topKfrequent.py
+++ topKfrequent.py
@@ -1,6 +1,16 @@
 from typing import List
 
 class Solution:
+
+    
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     count = {}
+    #     for num in nums:
+    #         count[num] = count.get(num, 0) + 1
+    #     count = sorted(count.items(), key=lambda x: x[1], reverse = True)
+    #     return [num for num, freq in count[:k]]
+    
+    
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
         freq = [[] for _ in range(len(nums) + 1)]
