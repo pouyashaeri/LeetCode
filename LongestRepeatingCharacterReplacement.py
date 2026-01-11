057f515253a5a5b64c83455b20d0a9cfc11c74d8
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2026-01-11 04:41:40 -0700

    characterReplacement Sol2

--- LongestRepeatingCharacterReplacement.py
+++ LongestRepeatingCharacterReplacement.py
@@ -1,16 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         freq = {}
-        left = 0
         max_freq = 0
-        result = 0
-
+        left = 0
+        max_len = 0
         for right in range(len(s)):
             freq[s[right]] = freq.get(s[right], 0) + 1
             max_freq = max(max_freq, freq[s[right]])
-            while (right - left + 1) - max_freq > k:
+            length = right - left + 1
+            while length - max_freq > k:
                 freq[s[left]] -= 1
-                left += 1                
-            result = max(result, right - left + 1)
-        
-        return result
+                left += 1
+                length = right - left + 1
+            if length - max_freq <= k:
+                max_len = max(max_len, length)
+        return max_len
