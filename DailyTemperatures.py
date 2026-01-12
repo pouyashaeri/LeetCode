50bc4118032f528fa5f9e5cf5524391bcac3f2b4
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2026-01-11 23:13:12 -0700

    daily temperature

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -2,15 +2,15 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0]*len(temperatures)
+        result = [0] * len(temperatures)
         stack = []
+        
         for today in range(len(temperatures)):
             today_temp = temperatures[today]
-
             while stack and today_temp > temperatures[stack[-1]]:
                 previous_day = stack.pop()
                 result[previous_day] = today - previous_day
-            
+
             stack.append(today)
-        
+
         return result
