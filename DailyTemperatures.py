320638c6ec7c3cf6ceb6f0cd8afcbe43f489bbc6
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2026-01-11 23:13:56 -0700

    reveresed polish practice

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -4,13 +4,12 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result = [0] * len(temperatures)
         stack = []
-        
+
         for today in range(len(temperatures)):
             today_temp = temperatures[today]
             while stack and today_temp > temperatures[stack[-1]]:
                 previous_day = stack.pop()
                 result[previous_day] = today - previous_day
-
             stack.append(today)
 
         return result
